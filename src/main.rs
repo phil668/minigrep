c554e30adc4679b56a463aac89e73085bb154ff0
liuyue <1308972436@qq.com>
2024-01-09 23:11:06 +0800

    test: add test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::{env, process};
+
+use minigrep::{run, Config};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments:{err}");
+        process::exit(1);
+    });
+
+    if let Result::Err(v) = run(config) {
+        println!("Application Error:{v}");
+        process::exit(1)
+    }
 }
